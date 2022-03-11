9920f78ce6a6a443da59e05571f940f05d163429
hbsgr <1399617205@qq.com>
2022-03-11 22:11:48 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author sgrstart
+ * @create 2022-03-11 21:38
+ */
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("hello git!");
+    }
+}
