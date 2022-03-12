462c4e70a6782962196461aa2b6d267422289c73
qwerty442 <81411564+qwerty442@users.noreply.github.com>
2022-03-12 11:02:13 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("master test!");
         System.out.println("hello test1");
+        System.out.println("pull test1");
 
 
     }
